3cc3362c8fba7d762f4e356aa111da95deb1f936
libbyschuknight <libbysk@gmail.com>
2015-06-27 17:10:34 +1200

    sorting out adding meal to meal plan

--- app/controllers/MealController.js
+++ app/controllers/MealController.js
@@ -3,6 +3,27 @@ app.controller("MealController", ["$scope", "meals", '$routeParams', function($s
     $scope.detail = data[$routeParams.id];
     console.log($scope.detail);
   });
+
+  $scope.AddToMealPlan = function() {
+    var meal = $scope.detail;
+
+    // var mealData = JSON.stringify(data);
+    console.log(meal.Id);
+
+    // $http({
+    //   method: "POST",
+    //   url:
+    // })
+
+
+
+  }
+
 }]);
+
+
+
+
+
 
 

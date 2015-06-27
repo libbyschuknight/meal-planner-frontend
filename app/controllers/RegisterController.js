68abd35d4e21d7c11a94f73176945fa681b73443
sleepysaurus <ylegaspi@gmail.com>
2015-06-27 16:41:52 +1200

    Get Current Users Meal Plan Currently In The Register Controller

--- app/controllers/RegisterController.js
+++ app/controllers/RegisterController.js
@@ -12,7 +12,7 @@ console.log("eferer")
 
         console.log(postdata);
 
-        $http.post("http://roameals.azurewebsites.net/api/Account/Register", postdata)
+        $http.post("http://localhost:62555/api/Account/Register", postdata)
         .success(function () {
             console.log("sucess.......")
               $window.location.href = '#Login';
@@ -22,7 +22,33 @@ console.log("eferer")
         });
 
     };
+    $scope.getMealPlan = function() {
+			 
+		var authoriz = 'Bearer ' + $window.sessionStorage.getItem('tokenKey');
+		$http({
+			    method: 'GET',
+			    url: 'http://roameals.azurewebsites.net/api/MealPlans',
+			    headers: {
+				          'Content-Type': 'application/json',
+				          'Authorization': authoriz
+			        	 }
+			 })
+
+        	.success(function(data){
+
+			console.log("fuck yeah")
+			console.log(data)
+
+    	})
+
+
+
+
+
+    }
 });
+
+
 
     // var DisplayLogin=function()
     // {

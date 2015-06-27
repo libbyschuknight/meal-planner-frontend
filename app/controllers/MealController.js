d88b0f7284a5dfe8b4d4be3bfa3476a860523d35
libbyschuknight <libbysk@gmail.com>
2015-06-27 20:46:27 +1200

    deleleting function out of meal controller that was moved to main controller

--- app/controllers/MealController.js
+++ app/controllers/MealController.js
@@ -3,25 +3,6 @@ app.controller("MealController", ["$scope", "meals", '$routeParams', function($s
     $scope.detail = data[$routeParams.id];
     console.log($scope.detail);
   });
-
-  $scope.AddToMealPlan = function() {
-    var meal = $scope.detail;
-    var id = meal.Id;
-    var authoriz = 'Bearer ' + $window.sessionStorage.getItem('tokenKey');
-
-    $http({
-      method: "POST",
-      url: "http://roameals.azurewebsites.net/api/MealPlans/AddTo",
-      data: id,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': authoriz
-      }
-    })
-    .success(function(data){
-      console.log(data);
-    })
-  }
 }]);
 
 
